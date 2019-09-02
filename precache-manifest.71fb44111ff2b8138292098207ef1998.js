d95f3122db994e607324d3ae31503daf49314e84
venanen <help.venanen@yandex.ru>
2019-09-02 23:49:11 +0300

    Add files via upload

---
+++ precache-manifest.71fb44111ff2b8138292098207ef1998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1a4ddfd1606d576b085027eb8e941e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e1a4c1bb2c2f7911cd9",
+    "url": "/static/css/2.78457e6a.chunk.css"
+  },
+  {
+    "revision": "55906dcf3e5ef4bf656b",
+    "url": "/static/css/main.28950db7.chunk.css"
+  },
+  {
+    "revision": "3e1a4c1bb2c2f7911cd9",
+    "url": "/static/js/2.f3cb005b.chunk.js"
+  },
+  {
+    "revision": "55906dcf3e5ef4bf656b",
+    "url": "/static/js/main.4e22ab66.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
